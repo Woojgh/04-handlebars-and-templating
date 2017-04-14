4f7027c5fbb9cebcad822bfb78bc9cce5985ea61
woojgh <jamessalamonsen@gmail.com>
2017-04-14 09:17:27 -0700

    Adding in half the todos

--- james/scripts/article.js
+++ james/scripts/article.js
@@ -14,10 +14,6 @@ function Article (rawDataObj) {
 Article.prototype.toHtml = function() {
   var templateScript = $('article-template').html();
   var theTemplate = Handlebars.compile(templateScript);
- 
-  var compiledHtml = theTemplate(rawDataObj);
-  $('#article-template').html(compiledHtml);
-
   // TODO: Use handlebars to render your articles.
   //       - Get your template from the DOM.
   //       - Now "compile" your template with Handlebars.
@@ -26,10 +22,12 @@ Article.prototype.toHtml = function() {
   //   Since your template can't hold any JS logic, we need to execute the logic here.
   //   The result is added to the object as a new property, which can then be referenced by key in the template.
   //   For example, you might want to display how old a post is, or say "(draft)" if it has no publication date:
-  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000);
   this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
 
   // TODO: Use the function that Handlebars gave you to return your filled-in html template for THIS article.
+  var compiledHtml = theTemplate(rawDataObj);
+  $('#article-template').html(compiledHtml);
 
 };
 
